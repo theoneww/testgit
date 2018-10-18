bcc064de11e7cdbeefb2321da258acbfcd8de276
wangwei <ww.it@qq.com>
2018-10-18 13:42:22 +0800

    Initial commit

---
+++ src/main/java/com/ww/testgit/demo/HelloGit.java
@@ -0,0 +1,14 @@
+package com.ww.testgit.demo;
+
+/**
+ * @ Author: ww
+ * @ Desc:
+ * @ Date: Create in 13:34 2018/10/18
+ */
+public class HelloGit {
+
+    public String helloGit(){
+        return "老子今天就要学会git！！！";
+    }
+
+}
